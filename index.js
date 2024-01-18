d48f8d377512f589f5c67648e8c17ded3ef3e9fc
Sahin2002 <shahinderishov02@iclound.com>
2024-01-17 18:16:03 -0800

    lesson3

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
-var name='sahin'
-var surname='derisov'
-let age=20
-const city='Baku'
-console.log('Name:'+name+'\n','Surname:'+surname+'\n','Age:'+age+'\n','City:'+city)
+const personalProfile={
+    firstname:'sahin',
+    firstsurname:'derisov',
+    age:21,
+    favoritLanguage:'Englis',
+    contactInfo:{
+        email:'shahinderishov@gmail.com',
+        phone:'0558049820'
+    }
+}
+personalProfile['occuptio']='Developer'
+personalProfile.contactInfo['address']='baku'
+console.log(personalProfile.firstname)
+console.log(personalProfile.favoritLanguage)
+console.log(personalProfile['contactInfo'].address)
